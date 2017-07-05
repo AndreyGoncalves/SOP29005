e7f122c215492285067e513c8da1575ef8210ee6
AndreyGoncalves <andreygoncalves@live.com>
2017-07-05 14:44:06 -0300

    Update FileSystem.h

--- PJ3/fs/FileSystem.h
+++ PJ3/fs/FileSystem.h
@@ -1,4 +1,11 @@
 #include "VirtualDisk.h"
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+
 
 class FileSystem {
 private:
@@ -17,14 +24,37 @@ public:
 
     VirtualDisk * disk() { return &_disk; }
 
-    int MAX_FILES=64;
+    int MAX_FILES=40;
     int BLOCKS_PER_FILE=1;
 
 private:
-	VirtualDisk _disk;
-	static FileSystem * _fs;
-	char buf2[VirtualDisk::BLOCK_SIZE];
-	int dir,dir_len,data_ind,used,length,first,count,num_blocks;
-	char name[64];
-	int fat[8191];
+    VirtualDisk _disk;
+    static FileSystem * _fs;
+    char buf2[VirtualDisk::BLOCK_SIZE];
+    
+    struct stc_file{
+        int32_t active;
+        int32_t first_block;
+        int32_t offset_read;
+        int32_t offset_write;
+           
+    };
+    
+    struct stc_bloco{
+	int32_t dir;
+	int32_t dir_len;
+	int32_t data_ind;
+    };
+
+    struct stc_diretorio{
+	int32_t used;
+	char name[20];
+	int32_t size;
+	int32_t head;
+	int32_t ref_count;
+	int32_t num_blocks;
+    };
+    
+    stc_diretorio* dir;
+    stc_bloco fsys;
 };
